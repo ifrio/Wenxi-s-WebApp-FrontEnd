b60af36ce34886535d35c17548515e4e94b61a49
Ishmael Rico <ishmaelrico@gmail.com>
2021-01-14 02:41:16 +0000

    Adding Flask functionality

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function predict() {
 
-	var username = $("#username_input").val();
+	var user_input = $("#user_input").val();
 
 	$.ajax({
 
-		url: "https://greedyrunnyshareware--sunyu912.repl.co/sentiment/" + business + "/" + location, success: function(result) {
+		url: "https://saddlebrowntalldemos.wenxigao.repl.co/prediction/" + user_input, success: function(result) {
 
-			$("#result_label").text(result);
+			$("#result").text(result);
 
 	}
 
