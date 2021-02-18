1d8c39d9e23489f009f34adc045ba10c5f3485b5
Ishmael Rico <ishmaelrico@gmail.com>
2021-02-18 02:42:56 +0000

    better front end, backend adjusted, needs ajax fix

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-function predict() {
+//Automatic Slideshow - change image every 3 seconds
+//var urls = ['https://www.youtube.com/embed/J3svfiJvyb0', 'https://www.youtube.com/embed/R0cAiT5Y7BY'];
+//var pos = 0;
 
-	var user_input = $("#user_input").val();
+//setTimeout(next(), 5000);
+//function next()
+//{
+//  if(pos == urls.length) pos = 0; // reset the counter
+//  document.getElementById('rotate').src = urls[pos];
+//  pos++;
+//}
+
+function recommendVideo() {
+
+	var user_input = document.getElementById("user_input").value;
 
 	$.ajax({
 
-		url: "https://saddlebrowntalldemos.wenxigao.repl.co/prediction/" + user_input, success: function(result) {
-
-			$("#result").text(result);
-
-	}
+		url: "https://wenxis-youtube-recommendation-system.ishmaelrico.repl.co/recommendVideo/" + user_input, 
+    success: function(result) {
+			$("#result_label").text(result);
+    }
 
 	});
 
